c53371ce613b57836762108457c4829e093cb302
aurore-dw <118165322+aurore-dw@users.noreply.github.com>
2023-11-20 15:59:18 +0100

    User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(["user_list", "user_details"])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(["user_list", "user_details"])]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(["user_details"])]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(["user_details"])]
+    private ?string $password = null;
+
+    #[ORM\ManyToOne(inversedBy: 'users')]
+    #[ORM\JoinColumn(name: 'client_id', referencedColumnName: 'id')]
+    #[Groups(["user_list", "user_details"])]
+    private ?Client $client = null;
+
+    #[ORM\Column(type : "json")]
+    #[Groups(["user_details"])]
+    private ?array $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function getUserIdentifier(): string
+    {
+    return $this->email;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getClient(): ?Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(?Client $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->role = $roles;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        
+    }
+
+    public function getSalt()
+    {
+    
+    }
+
+}
